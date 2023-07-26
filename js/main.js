0bf717581d75e4c2778740216bd409ab67a1776a
big-good-boy <drago999@bk.ru>
2023-07-26 10:04:49 +0500

    обновление

--- js/main.js
+++ js/main.js
@@ -11,16 +11,23 @@ function render(el) {
 	html.innerHTML = '';
 	html.innerHTML += `
 		<ul class="slider__list" style="height: ${Math.round(document.getElementById('slider').parentNode.clientWidth / 1.3333)}px;">
-			<li class='slider__item'>
-				<img class="slider__img" src='${imagesPath[el]}' alt=''>
+			<li class="slider__item">
+				<img class="slider__img" src="${imagesPath[el]}" alt="">
 			</li>
 		</ul>
 		<div class="slider__navigator">
 			<div class="slider__button slider__button--prev" onclick="prev(${el})"></div>
 			<div class="slider__button slider__button--next" onclick="next(${el})"></div>
 		</div>
-		<div class="slider__pagination"></div>
+		<ul class="slider__pagination">
+		</ul>
 	`;
+
+	for (const key in imagesPath) {
+		document.querySelector('.slider__pagination').innerHTML += `
+			<li class="slider__pagination-item" onclick="outputsTheTransmitted(${key});"></li>
+		`;
+	}
 }
 
 function prev(el) {
@@ -39,4 +46,18 @@ function next(el) {
 	}
 }
 
+function outputsTheTransmitted(num) {
+	document.querySelector('.slider__item').innerHTML = `<img class="slider__img" src="${imagesPath[num]}" alt="">`;
+	
+	const el = document.querySelectorAll('.slider__pagination-item');
+	el.forEach((element) => {
+		if (element.classList.contains('slider__pagination-item--active')) {
+			element.classList.remove('slider__pagination-item--active');
+		}
+	});
+	document.querySelectorAll('.slider__pagination-item')[num].classList.add('slider__pagination-item--active');
+}
+
 render(0);
+
+// передавать желаемое соотношение высоты к ширине
